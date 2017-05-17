999a182530c25e31c3fcaeee253908a486018374
Pegah Rashidnia <rash10@cancer-f5rdy12.osumc.edu>
2017-05-17 13:14:12 -0400

    separated reads with N and no N

--- undet_analysis.py
+++ undet_analysis.py
@@ -9,28 +9,54 @@ import seaborn as sns
 
 file = 'Undetermined_S0_L003_R1_001.fastq.gz'
 with gzip.open(file, 'rb') as f:
-	id = []
+	has_N = []
+	no_N = []
 	#Get barcode
 	for ln in f:
 		if ln.startswith("@"):
 			barcode = ln.rsplit(":", 1)
 			#print(barcode[1])
-			id.append(barcode[1].strip())
-		if len(id) == 1000:
-			break
+			bc = barcode[1].strip()
+			if "N" in bc:
+				has_N.append(bc)
+			else:
+				no_N.append(bc)
+#		if len(no_N) == 50:
+#			break
 
-#print(id)			
-df = pd.DataFrame({'barcodes': id})
+#Barcodes w/ N		
+df = pd.DataFrame({'barcodes': has_N})
 df['forward'], df['reverse'] = zip(*df['barcodes'].apply(lambda x: x.split('+', 1)))
-grouped = df.groupby(['forward','reverse'])["barcodes"].count().reset_index(name="count")
+grouped_N = df.groupby(['forward','reverse'])["barcodes"].count().reset_index(name="count")
 
 #print(df)
-print(grouped)
+print(grouped_N)
 
-pivoted = grouped.pivot(index="forward", columns="reverse")
+grouped_N.to_csv('count.csv', sep='\t')
+
+pivoted = grouped_N.pivot(index="forward", columns="reverse")
 pivoted.fillna(0, inplace=True)
 print(pivoted)
 
-fig, ax = plt.subplots()
-sns.heatmap(data=pivoted, ax=ax)
-sns.plt.show()
+
+#heatmap = sns.heatmap(data=pivoted)
+#fig = heatmap.get_figure()
+#fig.savefig('heatmap.png')
+
+
+
+#Barcodes w/o N
+df_noN = pd.DataFrame({'barcodes': no_N})
+df_noN['forward'], df_noN['reverse'] = zip(*df_noN['barcodes'].apply(lambda x: x.split('+', 1)))
+grouped_noN = df_noN.groupby(['forward','reverse'])["barcodes"].count().reset_index(name="count")
+
+print(grouped_noN)
+grouped_noN.to_csv('count_noN.csv', sep='\t')
+
+pivoted = grouped_noN.pivot(index="forward", columns="reverse")
+pivoted.fillna(0, inplace=True)
+print(pivoted)
+
+#heatmap = sns.heatmap(data=pivoted)
+#fig = heatmap.get_figure()
+#fig.savefig('heatmap_noN.png')
